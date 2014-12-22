e07b29a5b0da0d60a032727c62cdd40698d86497
SnareChops <snare_chops@yahoo.com>
2014-12-22 19:37:25 +0000

    Adding missing about controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Tweetful::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root :to => 'home#index'
 
+  get '/about' => 'about#index'
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131031152306) do
+ActiveRecord::Schema.define(version: 20131027233836) do
 
   create_table "follows", force: true do |t|
     t.integer  "user_id"
